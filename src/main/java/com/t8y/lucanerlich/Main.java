ee881bf8eed4d0f4ddaec6427f7323f085300909
lnerlich <business@lucanerlich.de>
2016-02-09 10:53:36 +0100

    added first driver - firefox. opens google as a test

--- src/main/java/com/t8y/lucanerlich/Main.java
+++ src/main/java/com/t8y/lucanerlich/Main.java
@@ -1,10 +1,21 @@
 package com.t8y.lucanerlich;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+import java.io.File;
+
 /**
  * Created by lnerlich on 09.02.2016.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        File pathToBinary = new File("C:\\Program Files (x86)\\Firefox Developer Edition\\firefox.exe");
+        FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
+        FirefoxProfile firefoxProfile = new FirefoxProfile();
+        WebDriver driver = new FirefoxDriver(ffBinary,firefoxProfile);
+        driver.get("http://www.google.com");
     }
 }
