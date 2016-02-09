491d10ad67e3e60279a7dcbb92e31d2dc00e5b68
lnerlich <business@lucanerlich.de>
2016-02-09 10:24:24 +0100

    Initial commit

---
+++ src/main/java/com/t8y/lucanerlich/Main.java
@@ -0,0 +1,10 @@
+package com.t8y.lucanerlich;
+
+/**
+ * Created by lnerlich on 09.02.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
