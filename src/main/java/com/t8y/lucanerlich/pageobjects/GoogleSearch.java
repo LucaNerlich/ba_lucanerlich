2953439cd1ab84b26a6c607bed66bdaeb3ef4f6d
Luca Nerlich <business@lucanerlich.de>
2016-03-11 23:14:24 +0100

    altered baseurl, added test googlesearch

---
+++ src/main/java/com/t8y/lucanerlich/pageobjects/GoogleSearch.java
@@ -0,0 +1,7 @@
+package com.t8y.lucanerlich.pageobjects;
+
+/**
+ * Created by lucan on 11.03.2016.
+ */
+public class GoogleSearch {
+}
---
+++ src/main/java/com/t8y/lucanerlich/tests/TestGoogleSearch.java
@@ -0,0 +1,7 @@
+package com.t8y.lucanerlich.tests;
+
+/**
+ * Created by lucan on 11.03.2016.
+ */
+public class TestGoogleSearch {
+}
